ee6f6ed4257d5409d675d1e99c2c23fc3807e824
veselinovStf <veselinovStf@gmail.com>
2020-10-17 06:08:24 +0300

    Difining project Architecture

---
+++ src/Presentation/FriendsOrganizer.UI/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using FriendsOrganizer.UI.Views;
+using System.Windows;
+
+namespace FriendsOrganizer.UI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+    }
+}
