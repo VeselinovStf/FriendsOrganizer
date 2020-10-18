fad15a38cc085670ebf960ebd050fface8008d5b
veselinovStf <veselinovStf@gmail.com>
2020-10-18 19:27:52 +0300

    Adding Message Box when changes are not commited

--- src/Presentation/FriendsOrganizer.UI/ViewModels/MainWindowViewModel.cs
+++ src/Presentation/FriendsOrganizer.UI/ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,7 @@ using FriendsOrganizer.UI.Models;
 using Prism.Events;
 using System;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace FriendsOrganizer.UI.ViewModels
 {
@@ -50,6 +51,17 @@ namespace FriendsOrganizer.UI.ViewModels
 
         private async void OnSelectedFriendEventHandler(int friendId)
         {
+            if (FriendDetailViewModel != null && FriendDetailViewModel.HasChange)
+            {
+                var result = MessageBox.Show("Are you shore to change friend? Your changes will be lost!", "Question",
+                    MessageBoxButton.OKCancel);
+
+                if (result == MessageBoxResult.Cancel)
+                {
+                    return;
+                }
+            }
+
             FriendDetailViewModel = this._friendDetailViewModelCreator();
 
             await this.FriendDetailViewModel.Load(friendId);
