295aa3ec52ec53d943690540e3dc80f69287dc1c
veselinovStf <veselinovStf@gmail.com>
2020-10-28 19:35:50 +0200

    Settup Pick List

--- src/Presentation/FriendsOrganizer.UI/ViewModels/MeetingDetailViewModel.cs
+++ src/Presentation/FriendsOrganizer.UI/ViewModels/MeetingDetailViewModel.cs
@@ -1,40 +1,109 @@
 ﻿using FriendOrganizer.Meetings.Service.Abstraction;
 using FriendsOrganizer.Data.Models;
+using FriendsOrganizer.Friends.Service.Abstraction;
+using FriendsOrganizer.UI.Models;
 using FriendsOrganizer.UI.ModelsWrappers;
 using FriendsOrganizer.UI.UIServices;
 using FriendsOrganizer.UI.ViewModels.Abstraction;
 using Prism.Commands;
 using Prism.Events;
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace FriendsOrganizer.UI.ViewModels
 {
     public class MeetingDetailViewModel : DetailViewModelBase, IMeetingDetailViewModel
     {
         private readonly IMeetingService _meetingService;
+        private readonly IFriendService _friendService;
         private readonly IMessageDialogService _messageDialogService;
+        private FriendModel _selectedAddedFried;
+        private FriendModel _selectedAvailibleFried;
         private MeetingModelWrapper _meeting;
+        private IEnumerable<Friend> _allFriends;
+
+        public ObservableCollection<FriendModel> AddedFriends { get; }
+        public ObservableCollection<FriendModel> AvailibleFriends { get; }
+
+        public ICommand AddFriendCommand { get; }
+        public ICommand RemoveFriendCommand { get; }
 
         public MeetingDetailViewModel(
             IMeetingService meetingService,
+            IFriendService friendService,
             IEventAggregator eventAggregator,
-            IMessageDialogService messageDialogService): base(eventAggregator)
+            IMessageDialogService messageDialogService) : base(eventAggregator)
         {
             this._meetingService = meetingService;
+            this._friendService = friendService;
             this._messageDialogService = messageDialogService;
-        }      
+            this.AddedFriends = new ObservableCollection<FriendModel>();
+            this.AvailibleFriends = new ObservableCollection<FriendModel>();
+
+            this.AddFriendCommand = new DelegateCommand(OnAddFriendExecute, OnAddFriendCanExecute);
+            this.RemoveFriendCommand = new DelegateCommand(OnRemoveFriendExecute, OnRemoveFriendCanExecute);
+        }
+
+        private bool OnRemoveFriendCanExecute()
+        {
+            return this.SelectedAddedFried != null;
+        }
+
+        private void OnRemoveFriendExecute()
+        {
+            //TODO
+        }
+
+        private bool OnAddFriendCanExecute()
+        {
+            return this.SelectedAvailibleFried != null;
+        }
+
+        private void OnAddFriendExecute()
+        {
+            //TODO
+        }
 
         public MeetingModelWrapper Meeting
         {
-            get 
-            { 
-                return _meeting; 
+            get
+            {
+                return _meeting;
             }
-            set 
-            { 
+            set
+            {
                 _meeting = value;
                 OnPropertyChanged();
+            }
+        }
+
+
+
+        public FriendModel SelectedAddedFried
+        {
+            get { return _selectedAddedFried; }
+            set
+            {
+                _selectedAddedFried = value;
+                OnPropertyChanged();
+                ((DelegateCommand)RemoveFriendCommand).RaiseCanExecuteChanged();
+            }
+        }
+
+
+
+        public FriendModel SelectedAvailibleFried
+        {
+            get { return _selectedAvailibleFried; }
+            set
+            {
+                _selectedAvailibleFried = value;
+                OnPropertyChanged();
+                ((DelegateCommand)AddFriendCommand).RaiseCanExecuteChanged();
             }
         }
 
@@ -44,6 +113,59 @@ namespace FriendsOrganizer.UI.ViewModels
             var newMeeting = await CreateMeeting();
 
             InitializeMeeting(newMeeting);
+
+
+
+        }
+
+        private async Task InitializePickList()
+        {
+            _allFriends = await this._friendService
+                .GetAllAsync();
+
+            SetupPickList();
+        }
+
+        private void SetupPickList()
+        {
+            var meetingFriendIds = Meeting
+                .Model
+                .FriendMeetings
+                .Select(fm => fm.Friend)
+                .Select(f => f.Id).ToList();
+
+            var addableFriends = _allFriends
+                .Where(f => meetingFriendIds.Contains(f.Id))
+                .OrderBy(f => f.FirstName);
+
+            var availibleFriends = _allFriends
+                .Except(addableFriends)
+                .OrderBy(f => f.FirstName);
+
+            AddedFriends.Clear();
+            AvailibleFriends.Clear();
+
+            foreach (var af in addableFriends)
+            {
+                AddedFriends.Add(new FriendModel()
+                {
+                    Id = af.Id,
+                    Email = af.Email,
+                    FirstName = af.FirstName,
+                    LastName = af.LastName
+                });
+            }
+
+            foreach (var af in availibleFriends)
+            {
+                AvailibleFriends.Add(new FriendModel()
+                {
+                    Id = af.Id,
+                    Email = af.Email,
+                    FirstName = af.FirstName,
+                    LastName = af.LastName
+                });
+            }
         }
 
         private async Task<Meeting> CreateMeeting()
@@ -64,7 +186,7 @@ namespace FriendsOrganizer.UI.ViewModels
                .GetAsync(id);
 
             InitializeMeeting(meetingServiceCall);
-          
+            await InitializePickList();
         }
 
         private void InitializeMeeting(Data.Models.Meeting meetingServiceCall)
