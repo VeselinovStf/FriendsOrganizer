51ed9846f6d468c1c35467aca040329971fbcd35
veselinovStf <veselinovStf@gmail.com>
2020-10-31 09:43:31 +0200

    Adding Programming Languages Details View

---
+++ src/Presentation/FriendsOrganizer.UI/ViewModels/ProgrammingLanguageDetailsViewModel.cs
@@ -0,0 +1,43 @@
+﻿using FriendsOrganizer.UI.UIServices;
+using FriendsOrganizer.UI.ViewModels.Abstraction;
+using Prism.Events;
+using System.Threading.Tasks;
+
+namespace FriendsOrganizer.UI.ViewModels
+{
+    public class ProgrammingLanguageDetailsViewModel : DetailViewModelBase
+    {
+        public ProgrammingLanguageDetailsViewModel(
+            IEventAggregator eventAggregator, 
+            IMessageDialogService messageDialogService) 
+            : base(eventAggregator, messageDialogService)
+        {
+        }
+
+        public override Task LoadAddableAsync()
+        {
+            return Task.Delay(0);
+        }
+
+        public override Task LoadAsync(int id)
+        {
+            Id = id;
+            return Task.Delay(0);
+        }
+
+        protected override void OnDeleteExecute()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override bool OnSaveCanExecute()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnSaveExecute()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Presentation/FriendsOrganizer.UI/Views/ProgrammingLanguageDetailsUserControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FriendsOrganizer.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for ProgrammingLanguageDetailsUserControl.xaml
+    /// </summary>
+    public partial class ProgrammingLanguageDetailsUserControl : UserControl
+    {
+        public ProgrammingLanguageDetailsUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
