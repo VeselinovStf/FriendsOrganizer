50d09016753c83cacfac647e8eb8e338b63b49fc
veselinovStf <veselinovStf@gmail.com>
2020-11-01 08:21:22 +0200

    Adding MahAps

--- src/Presentation/FriendsOrganizer.UI/Views/MainWindow.xaml.cs
+++ src/Presentation/FriendsOrganizer.UI/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using FriendsOrganizer.UI.ViewModels;
+using MahApps.Metro.Controls;
 using System.Windows;
 
 namespace FriendsOrganizer.UI.Views
@@ -6,7 +7,7 @@ namespace FriendsOrganizer.UI.Views
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         private readonly MainWindowViewModel _mainWindowViewModel;
 
